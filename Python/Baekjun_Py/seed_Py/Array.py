b37265bbb7769fa3d500ba47856c573759c122bb
kimminseo <kmsk030210@gmail.com>
2024-08-20 12:46:56 +0900

    array

--- Python/Baekjun_Py/seed_Py/Array.py
+++ Python/Baekjun_Py/seed_Py/Array.py
@@ -1,4 +1,28 @@
 # 10871 X 보다 작은 수
+'''
+ArrayNum, count = map(int, input().split())
+
+A = list(map(int, input().split()))
+
+for i in range(ArrayNum):
+    if A[i] < count:
+        print(A[i])
+'''
 # 10807 개수 세기
+'''
+Num = int(input())
+Arr = list(map(int, input().split()))
+Find = int(input())
+
+count = 0
+
+for i in Arr:
+    if i == Find:
+        count += 1
+print(count)
+'''
 # 5597 과제 안 내신 분..?
+
+
+
 # 2739 행렬 덧셈
