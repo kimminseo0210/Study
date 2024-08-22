ea47ba5e66ff6b6fd4dfd8f55e9ea00ead36c3d1
kimminseo <kmsk030210@gmail.com>
2024-08-22 09:49:33 +0900

    complete BassballGame

--- Python/CodeIt/BassballGame.py
+++ Python/CodeIt/BassballGame.py
@@ -1,5 +1,5 @@
 from random import randint
-'''
+
 def generate_numbers():
     numbers = []
     
@@ -10,7 +10,7 @@ def generate_numbers():
     
     print("0과 9 사이의 서로 다른 숫자 3개를 랜덤한 순서로 뽑았습니다.\n")
     return numbers
-'''
+
 def take_guess():
     print("숫자 3개를 하나씩 차례대로 입력하세요.")
     
@@ -27,5 +27,31 @@ def take_guess():
             new_guess.append(user_num)
     return new_guess
 
+def get_score(guesses, solution):
+    strike_count = 0
+    ball_count = 0
+    
+    for i in range(len(guesses)):
+        if guesses[i] in solution:
+            if guesses[i] == solution[i]:
+                strike_count += 1
+            else:
+                ball_count += 1
+    
+    return strike_count, ball_count
 
-print(take_guess())
+ANSWER = generate_numbers()
+tries = 0
+
+while True:
+    user_guess = take_guess()
+    
+    strike, ball = get_score(user_guess, ANSWER)
+    
+    print(f"{strike}S {ball}B")
+    tries += 1
+    
+    if strike == 3:
+        break
+
+print("축하합니다. {}번 만에 숫자 3개의 값과 위치를 모두 맞히셨습니다.".format(tries))
